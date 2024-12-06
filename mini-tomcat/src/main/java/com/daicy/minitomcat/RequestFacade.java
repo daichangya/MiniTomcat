9184bfca991523ac61292f8ca09570282b4e1166
daichangya <daichangya@163.com>
2024-12-06 10:34:00 +0800

    第12章代码提交

--- mini-tomcat/src/main/java/com/daicy/minitomcat/RequestFacade.java
+++ mini-tomcat/src/main/java/com/daicy/minitomcat/RequestFacade.java
@@ -1,9 +1,17 @@
 package com.daicy.minitomcat;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-public class RequestFacade extends HttpServletRequestWrapper {
+public class RequestFacade implements HttpServletRequest {
+
+    private HttpServletRequest httpServletRequest;
+
     /**
      * Constructs a request object wrapping the given request.
      *
@@ -11,6 +19,361 @@ public class RequestFacade extends HttpServletRequestWrapper {
      * @throws IllegalArgumentException if the request is null
      */
     public RequestFacade(HttpServletRequest request) {
-        super(request);
+        if (request == null) {
+            throw new IllegalArgumentException("Request cannot be null");
+        }
+        this.httpServletRequest = request;
+    }
+
+    public HttpServletRequest getHttpServletRequest() {
+        return httpServletRequest;
+    }
+
+    // HttpServletRequest methods
+    @Override
+    public AsyncContext getAsyncContext() {
+        return httpServletRequest.getAsyncContext();
+    }
+
+    @Override
+    public Object getAttribute(String s) {
+        return httpServletRequest.getAttribute(s);
+    }
+
+    @Override
+    public Enumeration<String> getAttributeNames() {
+        return httpServletRequest.getAttributeNames();
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return httpServletRequest.getCharacterEncoding();
+    }
+
+    @Override
+    public int getContentLength() {
+        return httpServletRequest.getContentLength();
+    }
+
+    @Override
+    public long getContentLengthLong() {
+        return httpServletRequest.getContentLengthLong();
+    }
+
+    @Override
+    public String getContentType() {
+        return httpServletRequest.getContentType();
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return httpServletRequest.getDispatcherType();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return httpServletRequest.getInputStream();
+    }
+
+    @Override
+    public String getLocalAddr() {
+        return httpServletRequest.getLocalAddr();
+    }
+
+    @Override
+    public String getLocalName() {
+        return httpServletRequest.getLocalName();
+    }
+
+    @Override
+    public int getLocalPort() {
+        return httpServletRequest.getLocalPort();
+    }
+
+    @Override
+    public Locale getLocale() {
+        return httpServletRequest.getLocale();
+    }
+
+    @Override
+    public Enumeration<Locale> getLocales() {
+        return httpServletRequest.getLocales();
+    }
+
+    @Override
+    public String getMethod() {
+        return httpServletRequest.getMethod();
+    }
+
+    @Override
+    public String getParameter(String s) {
+        return httpServletRequest.getParameter(s);
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        return httpServletRequest.getParameterMap();
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return httpServletRequest.getParameterNames();
+    }
+
+    @Override
+    public String[] getParameterValues(String s) {
+        return httpServletRequest.getParameterValues(s);
+    }
+
+    @Override
+    public String getProtocol() {
+        return httpServletRequest.getProtocol();
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return httpServletRequest.getReader();
+    }
+
+    @Override
+    public String getRealPath(String s) {
+        return httpServletRequest.getRealPath(s);
+    }
+
+    @Override
+    public String getRemoteAddr() {
+        return httpServletRequest.getRemoteAddr();
+    }
+
+    @Override
+    public String getRemoteHost() {
+        return httpServletRequest.getRemoteHost();
+    }
+
+    @Override
+    public int getRemotePort() {
+        return httpServletRequest.getRemotePort();
+    }
+
+    @Override
+    public RequestDispatcher getRequestDispatcher(String s) {
+        return httpServletRequest.getRequestDispatcher(s);
+    }
+
+    @Override
+    public String getScheme() {
+        return httpServletRequest.getScheme();
+    }
+
+    @Override
+    public String getServerName() {
+        return httpServletRequest.getServerName();
+    }
+
+    @Override
+    public int getServerPort() {
+        return httpServletRequest.getServerPort();
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return httpServletRequest.getServletContext();
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return httpServletRequest.isAsyncStarted();
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return httpServletRequest.isAsyncSupported();
+    }
+
+    @Override
+    public boolean isSecure() {
+        return httpServletRequest.isSecure();
+    }
+
+    @Override
+    public void removeAttribute(String s) {
+        httpServletRequest.removeAttribute(s);
+    }
+
+    @Override
+    public void setAttribute(String s, Object o) {
+        httpServletRequest.setAttribute(s, o);
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) throws UnsupportedEncodingException {
+        httpServletRequest.setCharacterEncoding(s);
+    }
+
+    @Override
+    public AsyncContext startAsync() throws IllegalStateException {
+        return httpServletRequest.startAsync();
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+        return httpServletRequest.startAsync(servletRequest, servletResponse);
+    }
+
+    // ServletRequest methods
+    @Override
+    public String getAuthType() {
+        return httpServletRequest.getAuthType();
+    }
+
+    @Override
+    public Cookie[] getCookies() {
+        return httpServletRequest.getCookies();
+    }
+
+    @Override
+    public long getDateHeader(String s) {
+        return httpServletRequest.getDateHeader(s);
+    }
+
+    @Override
+    public String getHeader(String s) {
+        return httpServletRequest.getHeader(s);
+    }
+
+    @Override
+    public Enumeration<String> getHeaders(String s) {
+        return httpServletRequest.getHeaders(s);
+    }
+
+    @Override
+    public Enumeration<String> getHeaderNames() {
+        return httpServletRequest.getHeaderNames();
+    }
+
+    @Override
+    public int getIntHeader(String s) {
+        return httpServletRequest.getIntHeader(s);
+    }
+
+    @Override
+    public String getPathInfo() {
+        return httpServletRequest.getPathInfo();
+    }
+
+    @Override
+    public String getPathTranslated() {
+        return httpServletRequest.getPathTranslated();
+    }
+
+    @Override
+    public String getContextPath() {
+        return httpServletRequest.getContextPath();
+    }
+
+    @Override
+    public String getQueryString() {
+        return httpServletRequest.getQueryString();
+    }
+
+    @Override
+    public String getRemoteUser() {
+        return httpServletRequest.getRemoteUser();
+    }
+
+    @Override
+    public boolean isUserInRole(String s) {
+        return httpServletRequest.isUserInRole(s);
+    }
+
+    @Override
+    public Principal getUserPrincipal() {
+        return httpServletRequest.getUserPrincipal();
+    }
+
+    @Override
+    public String getRequestedSessionId() {
+        return httpServletRequest.getRequestedSessionId();
+    }
+
+    @Override
+    public String getRequestURI() {
+        return httpServletRequest.getRequestURI();
+    }
+
+    @Override
+    public StringBuffer getRequestURL() {
+        return httpServletRequest.getRequestURL();
+    }
+
+    @Override
+    public String getServletPath() {
+        return httpServletRequest.getServletPath();
+    }
+
+    @Override
+    public HttpSession getSession() {
+        return httpServletRequest.getSession();
+    }
+
+    @Override
+    public String changeSessionId() {
+        return httpServletRequest.changeSessionId();
+    }
+
+    @Override
+    public HttpSession getSession(boolean create) {
+        return httpServletRequest.getSession(create);
+    }
+
+
+    @Override
+    public boolean isRequestedSessionIdValid() {
+        return httpServletRequest.isRequestedSessionIdValid();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromCookie() {
+        return httpServletRequest.isRequestedSessionIdFromCookie();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromURL() {
+        return httpServletRequest.isRequestedSessionIdFromURL();
+    }
+
+    @Override
+    public boolean isRequestedSessionIdFromUrl() {
+        return httpServletRequest.isRequestedSessionIdFromUrl();
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+        return false;
+    }
+
+    @Override
+    public void login(String username, String password) throws ServletException {
+
+    }
+
+    @Override
+    public void logout() throws ServletException {
+
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException, ServletException {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Part getPart(String name) throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException {
+        return null;
     }
 }
